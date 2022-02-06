87e774934308234adf25d999fbe6d859367d780b
Markus Grunwald <markus.grunwald74@gmail.com>
2022-02-06 17:45:12 +0100

    changing correct implementation of compression to the server

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3010;
 
-app.use(compression)
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
